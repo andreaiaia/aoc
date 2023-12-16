d3f99137809ef01bd8e4c58d5522556004a64375
Andrea Bianchi <bianan96@gmail.com>
2023-12-16 20:20:58 +0100

    Add boilerplate for day02

--- day02/src/bin/part1.rs
+++ day02/src/bin/part1.rs
@@ -9,7 +9,7 @@ fn main() {
 
 #[tracing::instrument]
 pub fn process(input: &str) -> anyhow::Result<u32> {
-    Ok(42)
+    Ok(8)
 }
 
 #[cfg(test)]
@@ -18,13 +18,14 @@ mod tests {
 
     #[test]
     fn test_process() -> anyhow::Result<()> {
-        let test_input = "1abc2
-            pqr3stu8vwx
-            a1b2c3d4e5f
-            treb7uchet";
+        let test_input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
         let result = process(test_input).expect("Test: not a u32 number");
-        assert_eq!(result, 42);
+        assert_eq!(result, 8);
 
         Ok(())
     }
